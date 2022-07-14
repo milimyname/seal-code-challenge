77d80dd7e525a0738b328b2cd84b52716e1d7cdf
mili <kamil.maksudov2019@gmail.com>
2022-07-14 02:18:37 +0200

    Added Img Folder

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -1,4 +1,4 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+le// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse, } from 'next'
 import formidable from 'formidable'
 import mime from 'mime'
