15a3d26f78f1000a3a698eb06b1f182ae677d100
mili <kamil.maksudov2019@gmail.com>
2022-07-14 02:23:08 +0200

    Updated Server Url

--- config/config.ts
+++ config/config.ts
@@ -1,5 +1,6 @@
 
 
 const dev = process.env.NODE_ENV !== "production";
-export const server = dev ? "http://localhost:3000" : "https://seal-code-challenge.vercel.app";
+// export const server = dev ? "http://localhost:3000" : "https://seal-code-challenge.vercel.app";
+export const server = dev ? "http://localhost:3000" : "https://seal-code-challenge.herokuapp.com";
 export const dir = dev ? "public" : "";
